e2ebaa84d7da6e6600bfeb7014466205d06858f5
abethebabe01 <abelamoreaux@gmail.com>
2025-05-15 15:16:15 -0700

    Fix MUI dependencies and bundling configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: true,
     rollupOptions: {
-      external: ['@mui/x-date-pickers', 'axios']
+      external: ['axios']
     }
   }
 })
